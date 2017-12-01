2d97994c4691b3a5d2b8388cee4d2f1ba234cd09
kanakl <ludek.kanak@seznam.cz>
2017-12-01 01:55:42 +0100

    Make instructions impl #11

---
+++ src/main/java/Instruction.java
@@ -0,0 +1,26 @@
+
+public class Instruction {
+	enum Color
+	{
+	    LIT,		//ulož konstantu A do zásobníku
+	    OPR, 		//proveď instrukci A
+	    LOD,		//ulož hodnotu proměnné z adr. L,A na vrchol zásobníku
+	    STO,		//zapiš do proměnné z adr. L,A hodnotu z vrcholu zásobníku
+	    CAL,		//volej proceduru A z úrovně L
+	    INT,		//zvyš obsah top-registru zásobníku o hodnotu A
+	    JMP,		//proveď skok na adresu A
+	    JMC,		//proveď skok na adresu A, je-li hodnota na vrcholu zásobníku 0
+	    RET;  		//návrat z procedury (return)
+	}
+	String funcName;
+	int level;
+	int address;
+	
+	public Instruction(String funcName, int level, int address) {
+		this.funcName = funcName;
+		this.level = level;
+		this.address = address;
+	}
+	
+	
+}
