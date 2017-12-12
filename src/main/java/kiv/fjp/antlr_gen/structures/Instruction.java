5e47819de3ffef6de0b0ab7d120493f0d17fea79
kanakl <ludek.kanak@seznam.cz>
2017-12-12 18:54:52 +0100

    Add getters and setters

--- src/main/java/kiv/fjp/antlr_gen/structures/Instruction.java
+++ src/main/java/kiv/fjp/antlr_gen/structures/Instruction.java
@@ -42,7 +42,31 @@ public class Instruction {
 		this.value = value;
 	}
 	
+	public IntType getIntstruction() {
+		return intstruction;
+	}
+
+	public void setIntstruction(IntType intstruction) {
+		this.intstruction = intstruction;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
 	public String toString() {
-		return intstruction.name()+ "\t" + level + " " + value;
+		return intstruction.name()+ "\t" + level + "\t" + value;
 	}
 }
