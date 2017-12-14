80f8a6da3dc7d612e86ef74a14c50d6a6c10e88e
kanakl <ludek.kanak@seznam.cz>
2017-12-14 13:14:38 +0100

    Interpreter #22
    Added OPR instructions

--- src/main/java/kiv/fjp/interpreter/Interpreter.java
+++ src/main/java/kiv/fjp/interpreter/Interpreter.java
@@ -7,67 +7,63 @@ import kiv.fjp.antlr_gen.structures.Instruction;
 public class Interpreter {
 	private static final int STACK_SIZE = 32;
 	private ArrayList<Instruction> instructions;
-	
+
 	private int base;
 	private int[] stack;
 	private int stackPointer;
 	private int instructionPointer;
-	
-	
+
 	public Interpreter(ArrayList<Instruction> instructions) {
 		this.instructions = instructions;
 		stack = new int[STACK_SIZE];
 		base = 1;
 		stackPointer = -1;
 	}
-	
+
 	public void runInterpret() {
-		while(instructionPointer != -1) {
-			
+		while (instructionPointer != -1) {
+
 			Instruction instruction = instructions.get(instructionPointer);
-			System.out.println(instructionPointer+" "+instruction.toString());
+			System.out.println(instructionPointer + " " + instruction.toString());
 			switch (instruction.getIntstruction()) {
 			case JMP:
 				instructionPointer = instruction.getValue();
 				break;
 			case JMC:
-				if(stack[stackPointer] == 0) {
+				if (stack[stackPointer] == 0) {
 					instructionPointer = instruction.getValue();
 				}
 				stackPointer--;
+				instructionPointer++;
 				break;
 			case OPR:
 				processOPR(instruction.getValue());
+				break;
 			case LIT:
 				stackPointer++;
 				stack[stackPointer] = instruction.getValue();
-				
 				instructionPointer++;
 				break;
 			case LOD:
-				stack[stackPointer] = stack[findNewBaseByLevel(instruction.getLevel())+instruction.getValue()];
+				stack[stackPointer] = stack[findNewBaseByLevel(instruction.getLevel()) + instruction.getValue()];
 				stackPointer++;
 				instructionPointer++;
 				break;
 			case STO:
-				
-				stack[findNewBaseByLevel(instruction.getLevel())+instruction.getValue()-1] = stack[stackPointer];
+				stack[findNewBaseByLevel(instruction.getLevel()) + instruction.getValue() - 1] = stack[stackPointer];
 				stackPointer--;
 				instructionPointer++;
 				break;
 			case CAL:
-				
 				stack[stackPointer + 1] = findNewBaseByLevel(instruction.getLevel());
 				stack[stackPointer + 2] = base;
 				stack[stackPointer + 3] = instructionPointer + 1;
-				base = stackPointer + 1 + 1; //TODO
+				base = stackPointer + 1 + 1; // TODO
 				instructionPointer = instruction.getValue();
-				
+
 				break;
 			case INT:
-				
-				if(stackPointer == -1) {
-					
+				if (stackPointer == -1) {
 					stack[stackPointer + 1] = 0;
 					stack[stackPointer + 2] = 0;
 					stack[stackPointer + 3] = -1;
@@ -81,57 +77,79 @@ public class Interpreter {
 				base = stack[base];
 				break;
 			}
-			System.out.println("Instruction: "+instructionPointer);
-			System.out.println("Base: "+ base);
+			System.out.println("Instruction: " + instructionPointer);
+			System.out.println("Base: " + base);
 			System.out.println("Stack pointer: " + (stackPointer + 1));
-			for(int i = 0; i <= stackPointer; i++) {
-				System.out.println(i+1 +" " + stack[i] );
+			for (int i = 0; i <= stackPointer; i++) {
+				System.out.println(i + 1 + " " + stack[i]);
 			}
 			System.out.println("---------------------------------------------");
 		}
 	}
-	private int findNewBaseByLevel(int level){
+
+	private int findNewBaseByLevel(int level) {
 		int newBase = base;
-		for(int i = 0; i < level; i++) {
+		for (int i = 0; i < level; i++) {
 			newBase = stack[newBase];
 		}
 		return newBase;
 	}
+
 	private void processOPR(int value) {
-		
-	
+
 		switch (Instruction.OPRType.values()[value]) {
 		case UNARY_MINUS:
+			stack[stackPointer] = -stack[stackPointer];
 			break;
 		case PLUS:
-			
+			stack[stackPointer - 1] += stack[stackPointer];
+			stackPointer--;
 			break;
 		case MINUS:
+			stack[stackPointer - 1] -= stack[stackPointer];
+			stackPointer--;
 			break;
 		case MULTI:
+			stack[stackPointer - 1] *= stack[stackPointer];
+			stackPointer--;
 			break;
 		case DIV:
+			stack[stackPointer - 1] /= stack[stackPointer];
+			stackPointer--;
 			break;
 		case MOD:
-			break;
-		case ODD:
+			stack[stackPointer - 1] %= stack[stackPointer];
+			stackPointer--;
 			break;
 		case TEST_EQ:
+			stack[stackPointer - 1] = (stack[stackPointer - 1] == stack[stackPointer]) ? 1 : 0;
+			stackPointer--;
 			break;
 		case TEST_NONEQ:
+			stack[stackPointer - 1] = (stack[stackPointer - 1] != stack[stackPointer]) ? 1 : 0;
+			stackPointer--;
 			break;
 		case LESS:
+			stack[stackPointer - 1] = (stack[stackPointer - 1] < stack[stackPointer]) ? 1 : 0;
+			stackPointer--;
 			break;
 		case GREATER_EQ:
+			stack[stackPointer - 1] = (stack[stackPointer - 1] >= stack[stackPointer]) ? 1 : 0;
+			stackPointer--;
 			break;
 		case GREATER:
+			stack[stackPointer - 1] = (stack[stackPointer - 1] > stack[stackPointer]) ? 1 : 0;
+			stackPointer--;
 			break;
-		case LESS_EQ:			
+		case LESS_EQ:
+			stack[stackPointer - 1] = (stack[stackPointer - 1] <= stack[stackPointer]) ? 1 : 0;
+			stackPointer--;
 			break;
-		case UNUSED:
+		default:
 			break;
 
 		}
+		instructionPointer++;
 	}
 
 }
