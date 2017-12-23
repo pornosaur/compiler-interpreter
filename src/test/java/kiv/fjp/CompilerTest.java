169e636872500787968f13d69fd220a6f86beda4
kanakl <ludek.kanak@seznam.cz>
2017-12-23 16:53:03 +0100

    Tests (basis) #48
    add generate file, comparing of lines

--- src/test/java/kiv/fjp/CompilerTest.java
+++ src/test/java/kiv/fjp/CompilerTest.java
@@ -1,17 +1,21 @@
 package kiv.fjp;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class CompilerTest {
 	protected static final String SAME_FILES = "Files have same content.";
+	protected static final String GEN_FILE = "generate/";
 	
 	public String testFiles(String testFilePath, String expResFilePath) throws IOException  {
 		
-		String genFilePath = testFilePath+"_output";
+		String genFilePath = GEN_FILE+testFilePath;
+		File file = new File(genFilePath);
 
+		file.getParentFile().mkdirs();
 		Main.compile(testFilePath, genFilePath);
 		
 		BufferedReader bwExpRes = new BufferedReader(new FileReader(expResFilePath));
@@ -27,6 +31,10 @@ public class CompilerTest {
             if(line1 == null || line2 == null) {
                 areEqual = false;
                 break;
+            }
+            if(!line1.equals(line2)) {
+            	areEqual = false;
+            	break;
             }
             	       
             line1 = bwExpRes.readLine();
