3fb41bdc594095a59c540ed5141428fbc19506c2
kanakl <ludek.kanak@seznam.cz>
2017-12-23 21:42:21 +0100

    Add getters and setters for InterpreterStep.java

--- src/main/java/kiv/fjp/interpreter/InterpreterStep.java
+++ src/main/java/kiv/fjp/interpreter/InterpreterStep.java
@@ -21,23 +21,63 @@ public class InterpreterStep {
 
 	@Override
 	public String toString() {
-		/*String step = instruction.toString() + "\n";
+		String step = instruction.toString() + "\n";
 		step += "Next instruction: " + nextInstruction + "\n";
 		step += "Base: " + base + "\n";
 		step += "Stack pointer: " + (stackPointer + 1) + "\n";
 		for (int i = 0; i <= stackPointer; i++) {
 			step += i + 1 + " " + stack[i] + "\n";
 		}
-		step += "---------------------------------------------\n";*/
-		String step = "expSteps.add(new InterpreterStep(instructions.get(), " + nextInstruction + ", " + base+", " + stackPointer+ ", new int[]{";
+		step += "---------------------------------------------\n";
+		/*String step = "expSteps.add(new InterpreterStep(instructions.get(), " + nextInstruction + ", " + base+", " + stackPointer+ ", new int[]{";
 		step += stack[0];
 		for (int i = 1; i <= stackPointer; i++) {
 			step += " ," + stack[i];
 		}
-		step += "}));\n";
+		step += "}));\n";*/
 		return step;
 	}
 	
+	public Instruction getInstruction() {
+		return instruction;
+	}
+
+	public void setInstruction(Instruction instruction) {
+		this.instruction = instruction;
+	}
+
+	public int getNextInstruction() {
+		return nextInstruction;
+	}
+
+	public void setNextInstruction(int nextInstruction) {
+		this.nextInstruction = nextInstruction;
+	}
+
+	public int getBase() {
+		return base;
+	}
+
+	public void setBase(int base) {
+		this.base = base;
+	}
+
+	public int getStackPointer() {
+		return stackPointer;
+	}
+
+	public void setStackPointer(int stackPointer) {
+		this.stackPointer = stackPointer;
+	}
+
+	public int[] getStack() {
+		return stack;
+	}
+
+	public void setStack(int[] stack) {
+		this.stack = stack;
+	}
+
 	@Override
     public boolean equals(Object obj) {
 		if(obj instanceof InterpreterStep) {
